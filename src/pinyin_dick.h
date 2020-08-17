e759cf39d1cb3591e38923697647b21788cf6ab0
Conero <conero@163.com>
2020-08-18 00:05:45 +0800

    创建结果体数组

---
+++ src/pinyin_dick.h
@@ -0,0 +1,15 @@
+// since: 2020-08-17
+// author: Joshua Conero
+
+// 有python 脚本生产
+// 拼音字典
+#ifndef _PINYIN_DICK_H_
+#define _PINYIN_DICK_H_
+
+#include "pinyin.h"
+
+// 结构体内容
+PINYIN PINYIN_DICK[50003] = 
+{
+    {"", "", "", ""}
+};
